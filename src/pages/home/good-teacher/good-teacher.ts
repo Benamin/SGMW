4b10af98a2029532ee3f4e49c70f626e988ae356
Benamin <1282546003@qq.com>
2019-09-09 21:07:40 +0800

    teacher update

--- src/pages/home/good-teacher/good-teacher.ts
+++ src/pages/home/good-teacher/good-teacher.ts
@@ -1,59 +1,66 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {HomeService} from "../home.service";
 import {LearnService} from "../../learning/learn.service";
 import {CommonService} from "../../../core/common.service";
 import {TeacherPage} from "../../learning/teacher/teacher";
+import {timer} from "rxjs/observable/timer";
 
 @Component({
-  selector: 'page-good-teacher',
-  templateUrl: 'good-teacher.html',
+    selector: 'page-good-teacher',
+    templateUrl: 'good-teacher.html',
 })
 export class GoodTeacherPage {
 
-  teacherList = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams,private homeSer:HomeService,
-              private learnSer:LearnService,private commonSer:CommonService) {
-  }
+    teacherList = [];
 
-  ionViewDidLoad() {
-    this.getGoodsTeacher();
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams, private homeSer: HomeService,
+                private learnSer: LearnService, private commonSer: CommonService) {
+    }
 
-  //优秀教师
-  getGoodsTeacher() {
-    this.homeSer.GetGoodTeacherList().subscribe(
-        (res) => {
-          this.teacherList = res.data.TeacherItems;
-        }
-    )
-  }
+    ionViewDidLoad() {
+        this.getGoodsTeacher();
+    }
 
-  async focusHandle(item) {
-    const data = {
-      TopicID: item.UserID
-    };
-    await this.learnSer.SaveSubscribe(data).subscribe(
-        (res) => {
-          this.commonSer.toast('关注成功');
-         this.getGoodsTeacher();
-        }
-    );
-  }
+    //优秀教师
+    getGoodsTeacher() {
+        this.homeSer.GetGoodTeacherList().subscribe(
+            (res) => {
+                this.teacherList = res.data.TeacherItems;
+            }
+        )
+    }
 
-  async cancleFocusHandle(item) {
-    console.log(item);
-    const data = {
-      TopicID: item.UserID
-    };
-    this.learnSer.CancelSubscribe(data).subscribe(
-        (res) => {
-          this.commonSer.toast('取消关注成功')
-          this.getGoodsTeacher();
+    doRefresh(e) {
+        this.getGoodsTeacher();
+        timer(800).subscribe(() => e.complete());
+    }
 
-        }
-    )
-  }
+    async focusHandle(item) {
+        const data = {
+            TopicID: item.UserID
+        };
+        await this.learnSer.SaveSubscribe(data).subscribe(
+            (res) => {
+                this.commonSer.toast('关注成功');
+                this.getGoodsTeacher();
+            }
+        );
+    }
+
+    async cancleFocusHandle(item) {
+        console.log(item);
+        const data = {
+            TopicID: item.UserID
+        };
+        this.learnSer.CancelSubscribe(data).subscribe(
+            (res) => {
+                this.commonSer.toast('取消关注成功')
+                this.getGoodsTeacher();
+
+            }
+        )
+    }
 
     //教师详情
     teachDetail(item) {
