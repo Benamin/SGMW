9d6785d77206cc856403807d5c0f51499ce1ea52
lishaoxing <674348576>
2019-09-22 01:49:20 +0800

    添加论坛列表

--- src/pages/forum/forum.component.ts
+++ src/pages/forum/forum.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'Page-forum',
+  selector: 'page-forum',
   templateUrl: './forum.component.html'
 })
 export class ForumPage implements OnInit {
