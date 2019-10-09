dab8b8255ebb95129ad4acc4638db593427a6708
lishaoxing <674348576>
2019-10-09 10:06:41 +0800

    修改 点击延迟问题

--- src/pages/forum/forum.service.ts
+++ src/pages/forum/forum.service.ts
@@ -237,8 +237,8 @@ export class ForumService {
       var oReq = new XMLHttpRequest();
 
       // export const UploadFilesSERVER_API_URL = '/api';
-      const UploadFilesSERVER_API_URL = 'http://devapi.chinacloudsites.cn/api';
-      // export const UploadFilesSERVER_API_URL = 'https://elearningapi.sgmw.com.cn/api';
+      const UploadFilesSERVER_API_URL = 'http://devapi.chinacloudsites.cn/api';  //测试
+      // export const UploadFilesSERVER_API_URL = 'https://elearningapi.sgmw.com.cn/api'; // 正式
 
       oReq.open("POST", UploadFilesSERVER_API_URL + "/Upload/UploadFiles", true);
       oReq.onload = function (oEvent: any) {
