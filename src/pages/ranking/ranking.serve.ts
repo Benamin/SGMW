b26c6bb5aefa5a59bd069ed6f3daa839addcf658
lishaoxing <674348576@qq.com>
2020-02-29 23:35:26 +0800

    还原版本

--- src/pages/ranking/ranking.serve.ts
+++ src/pages/ranking/ranking.serve.ts
@@ -23,34 +23,4 @@ export class RankingService {
     getExamTopList(){
         return this.http.get(SERVER_API_URL + '/exam/getExamTopList');
     }
-
-    // 加载更多讲师头衔
-    badgeteachertitlelist(data){
-        return this.http.post(SERVER_API_URL + '/forum/userbadge/badgeteachertitlelist',data);
-    }
-    // 加载更多讲师活跃度
-    teacherpostlist(data){
-        return this.http.post(SERVER_API_URL + '/forum/userbadge/teacherpostlist',data);
-    }
-
-    // 加载更多讲师评论榜单
-    teachscorelist(data){
-        return this.http.post(SERVER_API_URL + '/forum/userbadge/teachscorelist',data);
-    }
-    
-    // 加载更多学员榜
-    studenttitlelist(data){
-        return this.http.post(SERVER_API_URL + '/forum/userbadge/studenttitlelist',data);
-    }
-    // 加载更多学员活跃度
-    studentpostlist(data){
-        return this.http.post(SERVER_API_URL + '/forum/userbadge/studentpostlist',data);
-    }
-
-    // 加载更多学员完成课程数
-    studentstudylist(data){
-        return this.http.post(SERVER_API_URL + '/forum/userbadge/studentstudylist',data);
-    }
-    
-    
 }
