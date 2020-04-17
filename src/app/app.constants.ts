b215bfce4ba788a81e8591ae14b8f0f586ba273b
Benamin <1282546003@qq.com>
2020-04-17 10:38:02 +0800

    update bug

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -8,22 +8,22 @@ export const SERVER_API_URL = '/api';
 export const sgmw_client_id = "2961C96D-4DB0-4FCF-99FA-FE18AC9A496A";
 
 //通过判断url来跳转内部还是外部
-export const SERVER_HTTP_URL = 'https://sitportal.chinacloudsites.cn/dist/#/courseDetail/'; //测试
-// export const SERVER_HTTP_URL = 'https://elearning.sgmw.com.cn/#/courseDetail';  //生产
+// export const SERVER_HTTP_URL = 'https://sitportal.chinacloudsites.cn/dist/#/courseDetail/'; //测试
+export const SERVER_HTTP_URL = 'https://elearning.sgmw.com.cn/#/courseDetail';  //生产
 /*****end******/
 
 /***骏客***/
 //环境
-// export const JunKe_HTTP_URL = "/JunKeAPI";  //代理地址
-export const JunKe_HTTP_URL = "https://nbjtest.sgmw.com.cn";   //测试
+export const JunKe_HTTP_URL = "/JunKeAPI";  //代理地址
+// export const JunKe_HTTP_URL = "https://nbjtest.sgmw.com.cn";   //测试
 // export const JunKe_HTTP_URL = "https://nbj.sgmw.com.cn";   //生产
 
 //秘钥
 //开发环境
-export const JunKe_PRIVATE_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCZs0vnjp+z/6ggXS5jXzusSAfDO6OUDRNpZ/YDtLDHof8J0Z2O2Y0JeBcsItIwmoYMuaBYlMxGq2NJ4hZU4cSrHfpE7mt+QFgdJe2H2v8GYOi+umM56QGq+gM18jYsOVwZQXfVmAV+y8gVO+0Ksg77vab/QRZaYuPfyGyGEQTklwIDAQAB";
+// export const JunKe_PRIVATE_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCZs0vnjp+z/6ggXS5jXzusSAfDO6OUDRNpZ/YDtLDHof8J0Z2O2Y0JeBcsItIwmoYMuaBYlMxGq2NJ4hZU4cSrHfpE7mt+QFgdJe2H2v8GYOi+umM56QGq+gM18jYsOVwZQXfVmAV+y8gVO+0Ksg77vab/QRZaYuPfyGyGEQTklwIDAQAB";
 
 //生产环境
-// export const JunKe_PRIVATE_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCboECH8BMV6e8/DsEd9uyUqVQLpeEP1qkncIPWxlGn81++jpHjUohTku1k6Qakyl9tbxN+6mMBt6+rJD6/qDSGxXpx1hw3/u1bdTUfgk1DkqleMs4q4bZgwolYNqeFxVMkOMfcYDr8b9F1f4BA2Ezsjnr/x2XVu4dtGM9yFR7m+QIDAQAB";
+export const JunKe_PRIVATE_KEY = "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCboECH8BMV6e8/DsEd9uyUqVQLpeEP1qkncIPWxlGn81++jpHjUohTku1k6Qakyl9tbxN+6mMBt6+rJD6/qDSGxXpx1hw3/u1bdTUfgk1DkqleMs4q4bZgwolYNqeFxVMkOMfcYDr8b9F1f4BA2Ezsjnr/x2XVu4dtGM9yFR7m+QIDAQAB";
 
 export const JunKe_client_id = "41BC3798-5392-49A4-A191-0891F322920B";
 /*****end******/
@@ -36,20 +36,20 @@ export const XSZS_HTTP_URL = "/XSZSAPI";  //代理地址
 // export const XSZS_HTTP_URL = "https://promotion.sgmw.com.cn/API";  //生产
 
 //appid  登录销售助手接口使用
-export const XSZS_appId = "npuehtqbtp1puya9obgb";  //测试
-// export const XSZS_appId = "5nvi2v72vepo1p62a2vc";   //生产
+// export const XSZS_appId = "npuehtqbtp1puya9obgb";  //测试
+export const XSZS_appId = "5nvi2v72vepo1p62a2vc";   //生产
 
 //appKey 登录销售助手接口使用
-export const XSZS_appKey = "3s78yo1pv1pucentst5rl3jvbyl6iwmn";  //测试
-// export const XSZS_appKey = "do5d9txfvtdt7neftljltgxa1pcpuate";   //生产
+// export const XSZS_appKey = "3s78yo1pv1pucentst5rl3jvbyl6iwmn";  //测试
+export const XSZS_appKey = "do5d9txfvtdt7neftljltgxa1pcpuate";   //生产
 
 //client_id
 export const XSZS_client_id = "138B0798-8D43-4588-B291-DB8545865C8A";
 /*****end******/
 
 /***服务助手***/
-// export const FWZS_HTTP_URL = "FWZSAPI";
-export const FWZS_HTTP_URL = "https://sgmwsa.shaoxingzhuoyue.com/api";
+export const FWZS_HTTP_URL = "FWZSAPI";
+// export const FWZS_HTTP_URL = "https://sgmwsa.shaoxingzhuoyue.com/api";
 
 //appid
 export const FWZS_appid = "52492bf5765840b192fac6c7ca3d10c8";
