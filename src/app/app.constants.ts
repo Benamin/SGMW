90adcd0a75df845882e806c662142bbf48f24e38
Benamin <1282546003@qq.com>
2020-07-02 17:20:10 +0800

    v2.0.0

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,10 +1,10 @@
 // export let env = 'localhost';
 // export let env = 'dev';
-export let env = 'uat';
-// export let env = 'prod';
+// export let env = 'uat';
+export let env = 'prod';
 
-let keyEnv = 'dev';
-// let keyEnv = 'prod';
+// let keyEnv = 'dev';
+let keyEnv = 'prod';
 
 // /***培训平台***/
 export const SERVER_API_URL_LOCALHOST = '/api';
@@ -84,7 +84,7 @@ export const defaultImg = './assets/imgs/default.jpg';
 export const defaultHeadPhoto = './assets/imgs/userDefault.jpg';
 
 export const pageSize = 10;
-export const LastVersion = '1.10.0';
+export const LastVersion = '2.0.0';
 
 //提示信息
 export const NoUserMsg = "您的用户信息暂未同步到骏菱学社，请稍后再试。如有问题，请致电0772-2650611。";
