be4735ffaca141334131c29af91f8674b7279bf4
zengtiansheng117@163.com <zengtiansheng117@163.com>
2020-10-28 21:20:32 +0800

    最新 修改

--- src/pages/home/advanced/level/level.ts
+++ src/pages/home/advanced/level/level.ts
@@ -99,21 +99,11 @@ export class AdvancedLevelPage {
                     let loading = this.loadCtrl.create({
                         content: ''
                     });
-                    
-                    // this.homeSer.UpdateInitializeLevel({ leveltype: this.page.roleList[i].value }).subscribe(
-                    //     (resRole) => {
-                    //         if (resRole.code === 200) {
-                    //             this.page.leveltype = this.page.roleList[i].value;
-                    //             this.page.levelTypeText = this.page.roleList[i].label;
-                    //             loading.dismiss();
-                    //             this.getAdvancedLevel();
-                    //         }
-                    //     }
-                    // )
+
                     this.homeSer.ValidationLevel({}).subscribe(
                         (res) => {
                             // console.log('goAdvancedLevel', res.data, res.data.status === 1);
-                            // if (res.data.status === 1) { // 判断是1 else 是0 这里模拟方便
+                            if (res.data.status === 1) { // 判断是1 else 是0 这里模拟方便
                                 this.homeSer.InitializeLevel({ leveltype: this.page.roleList[i].value }).subscribe(
                                     (resInit) => {
                                         if (resInit.code === 200) {
@@ -126,19 +116,12 @@ export class AdvancedLevelPage {
                                         }
                                     }
                                 )
-                            // } else {
-                            //     this.homeSer.GetRoleByPCode({ code: 'Certification' }).subscribe(
-                            //         (resRole) => {
-                            //             // console.log('GetRoleByPCode', resRole.data);
-                            //             if (resRole.code === 200) {
-                            //                 this.page.leveltype = this.page.roleList[i].value;
-                            //                 this.page.levelTypeText = this.page.roleList[i].label;
-                            //                 loading.dismiss();
-                            //                 this.getAdvancedLevel();
-                            //             }
-                            //         }
-                            //     )
-                            // }
+                            } else {
+                                this.page.leveltype = this.page.roleList[i].value;
+                                this.page.levelTypeText = this.page.roleList[i].label;
+                                loading.dismiss();
+                                this.getAdvancedLevel();
+                            }
                         }
                     )
                 }
