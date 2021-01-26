171c0f0ec07ef8d72ebd72f21993433f2abf064b
Benamin <1282546003@qq.com>
2021-01-26 15:03:05 +0800

    微信分享朋友圈

--- src/components/share-wx/share-wx.ts
+++ src/components/share-wx/share-wx.ts
@@ -1,6 +1,7 @@
 import {Component, Type} from '@angular/core';
 import {NavParams, ViewController} from "ionic-angular";
 import {CommonService} from "../../core/common.service";
+import {PCURL} from "../../app/app.constants";
 
 declare var Wechat;
 
@@ -28,17 +29,35 @@ export class ShareWxComponent {
      * @param type wx=微信好友 pyq=微信朋友圈
      */
     shareWX(type) {
+        let img = this.data.Images;
+        let description = this.data.ContentWithoutHtml;
+        let thumb = '';
+
+        if (description.length > 100) {
+            description = description.slice(0, 100);
+        }
+        if (img && img.length > 0) {
+            thumb = img[0].Src || '';
+        }
+
+        const pcUrl = PCURL;
+        const obj = {
+            Title: this.data.Title,
+            description: description,
+            thumb: thumb,
+            webpageUrl: `${pcUrl}bbsdetails/${this.data.Id}`
+        }
         Wechat.share({
             message: {
-                title: this.data.Title, // 标题
-                description: this.data.description, // 简介
-                thumb: this.data.thumb, //帖子图片
+                title: obj.Title, // 标题
+                description: obj.description, // 简介
+                thumb: obj.thumb, //帖子图片
                 mediaTagName: "TEST-TAG-001",
                 messageExt: "这是第三方带的测试字段",
                 messageAction: "<action>dotalist</action>",
                 media: {
                     type: Wechat.Type.WEBPAGE,
-                    webpageUrl: this.data.webpageUrl   //打开的路径
+                    webpageUrl: obj.webpageUrl   //打开的路径
                 }
             },
             scene: type === 'wx' ? Wechat.Scene.SESSION : Wechat.Scene.TIMELINE   //SESSION=好友  朋友圈=TIMELINE
