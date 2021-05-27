9f24d544c9cc560f0c87f2dd49b87fc31efc580d
benjamin <1282546003@qq.com>
2021-05-27 16:42:29 +0800

    【update】课程

--- src/pipes/pipes.module.ts
+++ src/pipes/pipes.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { StringSlicePipe } from './string-slice/string-slice';
-import { LineFeedPipe } from './line-feed/line-feed';
-import { SecondFormatPipe } from './second-format/second-format';
-import { ToCeilPipe } from './to-ceil/to-ceil';
-import { TimeFormatPipe } from './time-format/time-format';
+import {NgModule} from '@angular/core';
+import {StringSlicePipe} from './string-slice/string-slice';
+import {LineFeedPipe} from './line-feed/line-feed';
+import {SecondFormatPipe} from './second-format/second-format';
+import {ToCeilPipe} from './to-ceil/to-ceil';
+
 @NgModule({
-	declarations: [StringSlicePipe,
-    LineFeedPipe,
-    SecondFormatPipe,
-    ToCeilPipe,
-    TimeFormatPipe],
-	imports: [],
-	exports: [StringSlicePipe,
-    LineFeedPipe,
-    SecondFormatPipe,
-    ToCeilPipe,
-    TimeFormatPipe]
+    declarations: [StringSlicePipe,
+        LineFeedPipe,
+        SecondFormatPipe,
+        ToCeilPipe,
+    ],
+    imports: [],
+    exports: [StringSlicePipe,
+        LineFeedPipe,
+        SecondFormatPipe,
+        ToCeilPipe,
+    ]
 })
-export class PipesModule {}
+export class PipesModule {
+}
