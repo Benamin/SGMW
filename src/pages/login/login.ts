5699f53b9cac498259abf024527650ec5799a88c
benjamin <1282546003@qq.com>
2021-05-30 18:15:17 +0800

    【update】v3.6.01

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -729,7 +729,6 @@ export class LoginPage {
     updateRegID(res) {
         this.jPush.getRegistrationID().then((regiID) => {
             if (regiID) {
-                console.log("登陆 jpushId", regiID);
                 this.RegiID = regiID;
                 this.uploadRegID(res);
             } else {
